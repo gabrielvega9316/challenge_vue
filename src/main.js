f693b8ed3d2192344c731419b8be0ba4a1275200
Gabriel Vega <gabrielvega9316@gmail.com>
2023-06-13 16:54:32 -0300

    init vuex

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import './assets/main.css'
-
+import { createStore } from 'vuex';
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+const store = createStore({
+    // Configuración de tu store Vuex
+  });
 const app = createApp(App)
-
+app.use(store);
 app.use(router)
 
 app.mount('#app')
